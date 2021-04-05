3c49d80025641b4bf7e88b59d173f032e632af5b
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-04 23:13:46 -0400

    Array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching(arrDrivers = [], strName) {
+    return arrDrivers.filter(function(el) {
+        return el.toLowerCase().indexOf(strName.toLowerCase()) !== -1;
+    });
+}
+
+function fuzzyMatch(arrDrivers = [], strChar) {
+    return arrDrivers.filter(function(el) {
+        return el.toLowerCase().indexOf(strChar.toLowerCase()) == 0;
+    });
+}
+
+function matchName(arrDrivers, driverName) {
+    var arrResult = [];
+    arrResult = arrDrivers.filter(function(el) {
+        return el.name.toLowerCase().indexOf(driverName.toLowerCase()) !== -1;
+    })
+    return arrResult;
+}
